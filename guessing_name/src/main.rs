fbdfdbba706945666979d69bbcd20256230585fc
Aditya <37655818+adi-075@users.noreply.github.com>
2023-03-05 00:18:35 +0530

    Add Random Numbers to Guessing Game

--- guessing_name/src/main.rs
+++ guessing_name/src/main.rs
@@ -1,8 +1,15 @@
 //Importing the IO Library part of the standard library
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
+
 fn main() {
     //Entry point
     println!("Welcome to the guessing game!"); //println! macro
+
+    let secret_number = rand::thread_rng().gen_range(1..=100); //start..=end
+    println!("The secret number is: {secret_number}");
+
     println!("Please input your guess!");
 
     let mut guess = String::new(); //String::new, a function that returns a new instance of a String
@@ -15,8 +22,16 @@ fn main() {
         a way to let multiple parts of your code access one piece of data without needing to copy that data into memory multiple times.*/
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please input your number!");
+
     // Same as
     //io::stdin().read_line(&mut guess).expect("Failed to read line");
 
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too Small!"),
+        Ordering::Greater => println!("Too Large!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
