0c709c4bff27517c8e7ddb48402e4ce0ce0316e7
Aditya <37655818+adi-075@users.noreply.github.com>
2023-03-05 12:30:17 +0530

    Add Data Types

--- common_concepts/src/main.rs
+++ common_concepts/src/main.rs
@@ -27,7 +27,7 @@ fn main() {
 
     //Learning about Variable Shadowing
     let y = 12;
-    //Using the let keyword makes a new variable and doesn't make the previous variable mut
+    //Using the let keyword effectively makes a new variable and doesn't make the previous variable mut
     let y = y + 1;
     {
         let y = y * 2;
@@ -39,4 +39,33 @@ fn main() {
     let spaces = "   ";
     let spaces = spaces.len();
     println!("Spaces: {}", spaces);
+
+    //TODO: Data Types in Rust
+    //Rust is a statically-typed language
+    //It must know the type during compile time
+
+    //When many types are possible, we must add type notation
+    let guess: u32 = "42".parse().expect("Not a number!");
+    println!("{}", guess);
+
+    //FIXME: Will throw errors
+    // let guess = "42".parse().expect("Not a number!");
+    // println!("{}", guess);
+
+    //Scalar - Single Value
+    // Integers
+    // i32 - 32-bit Integer
+    // u32 - usigned 32-bit integer
+    //size of i and u can be 2^n
+    // u8, u16, u32, u64, u128
+
+    //Char
+    let c = 'z';
+    println!("{}", c);
+
+    let z: char = 'e';
+    println!("{}", z);
+
+    let heart_eyed_cat = '😻';
+    println!("{}", heart_eyed_cat);
 }
